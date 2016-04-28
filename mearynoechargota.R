eec4f14a77eaab9d47d34d72ccde9d821ebd9bd4
scastillo <scastlara@gmail.com>
2016-04-28 17:29:02 +0200

    ASNDAS d

--- mearynoechargota.R
+++ mearynoechargota.R
@@ -1,8 +1,29 @@
 library(SummarizedExperiment)
+library(edgeR)
 
 se <- readRDS(file.path("Documentos/MASTER/IEO/KIDNEY/seKIRC.rds"))
 se
 
 dim(colData(se))
+clvar <- colnames(colData(se))
+mcols(colData(se), use.names=TRUE)
 
-# colnames(colData(se))
+rowRanges(se)
+
+dge <- DGEList(counts=assays(se)$counts, genes=mcols(se))
+assays(se)$logCPM <- cpm(dge, log=TRUE, prior.count=0.5)
+assays(se)$logCPM
+
+
+# MA PLOT NO NORM
+dge$samples$group <- se$type
+table(dge$samples$group)
+plotSmear(dge, lowess=TRUE)
+
+
+# MA PLOT NORM
+dgenorm <- calcNormFactors(dge)
+dgenorm$samples$group <- se$type
+table(dgenorm$samples$group)
+plotSmear(dgenorm, lowess=TRUE)
+
