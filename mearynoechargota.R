d1d229dd11dab848d77678f3bb268ed5bfa62c1b
scastillo <scastlara@gmail.com>
2016-04-28 18:50:37 +0200

    NOTHING

--- mearynoechargota.R
+++ mearynoechargota.R
@@ -1,5 +1,6 @@
 library(SummarizedExperiment)
 library(edgeR)
+library(ggplot2)
 
 se <- readRDS(file.path("Documentos/MASTER/IEO/KIDNEY/seKIRC.rds"))
 se
@@ -12,7 +13,11 @@ rowRanges(se)
 
 dge <- DGEList(counts=assays(se)$counts, genes=mcols(se))
 assays(se)$logCPM <- cpm(dge, log=TRUE, prior.count=0.5)
-assays(se)$logCPM
+
+
+# NORMALIZATION
+dgenorm <- calcNormFactors(dge)
+assays(se)$logCPMnorm <- cpm(dgenorm, log=TRUE, prior.count=0.5)
 
 
 # MA PLOT NO NORM
@@ -22,8 +27,27 @@ plotSmear(dge, lowess=TRUE)
 
 
 # MA PLOT NORM
-dgenorm <- calcNormFactors(dge)
 dgenorm$samples$group <- se$type
 table(dgenorm$samples$group)
 plotSmear(dgenorm, lowess=TRUE)
+
+
+# GENE DIST
+
+# NO NORM
+genemean <- data.frame(Means=rowMeans(assays(se)$logCPM[,-1]))
+
+ggplot(genemean) + 
+  geom_bar(aes(x=Means), binwidth = 1.5, fill="white", color="black") +
+  theme_bw() + xlab("\nlog2CPM") + ylab("Count\n") +
+  geom_vline(xintercept=mean(genemean$Means), color="red")
+
+# NORM
+
+genemean.norm <- data.frame(Means=rowMeans(assays(se)$logCPMnorm[,-1]))
+
+ggplot(genemean.norm) + 
+  geom_bar(aes(x=Means), binwidth = 1.5, fill="white", color="black") +
+  theme_bw() + xlab("\nlog2CPM") + ylab("Count\n") +
+  geom_vline(xintercept=mean(genemean.norm$Means), color="red")
 
